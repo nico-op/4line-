2bb6ffe84e009c3eb27357473471c63c53fca935
Gabriel Lobo <kevinlobo1920k@gmail.com>
2023-03-04 22:14:06 -0600

    refactorización función viabilidad

--- logica-funcional.rkt
+++ logica-funcional.rkt
@@ -1,7 +1,6 @@
 #lang racket/gui
 
-
-;hola 
+ 
 (define (suma-valores valores)
   (cond ((null? valores) 0)
         (else (+ (car valores) (suma-valores (cdr valores))))))
@@ -14,10 +13,5 @@
   (cond ((> (suma-valores candidate) 0) #t)
         (else #f)))
 
-
-(define (posicion link)
-  (cond ((equal? link 0) 9)
-        (else (10))))
-;; este cambio a realizar mientras se sube otro
 ;; como usar:
 ;; (viabilidad '(-1 0 1 3 4 5))
