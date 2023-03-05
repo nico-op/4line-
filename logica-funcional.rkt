2881b893927faf72d5cb0e8fcdbebb679b51b0f8
Gabriel Lobo <kevinlobo1920k@gmail.com>
2023-03-04 21:49:39 -0600

    cambio en la funcion viabilidad

--- logica-funcional.rkt
+++ logica-funcional.rkt
@@ -4,8 +4,10 @@
   (cond ((null? values) 0)
         (else (+ (car values) (sum-values (cdr values))))))
 
-(define (is-viable candidate)
+
+;; viabilidad: determina la viabilidad
+(define (viabilidad candidate)
   (cond ((> (sum-values candidate) 0) #t)
         (else #f)))
-
-
+;;como usar:
+;;(viabilidad '(-1 0 1 3 4 5))
