3d91e56a52848ac9a66140a8c66698b1301cd258
Nico Otárola <niicoporras@gmail.com>
2023-03-04 21:59:44 -0600

    Variables

--- logica-funcional.rkt
+++ logica-funcional.rkt
@@ -1,15 +1,15 @@
 #lang racket/gui
 
-(define (sum-values values)
-  (cond ((null? values) 0)
-        (else (+ (car values) (sum-values (cdr values))))))
+(define (suma-valores valores)
+  (cond ((null? valores) 0)
+        (else (+ (car valores) (suma-valores (cdr valores))))))
 
 
 ;; viabilidad: determina la viabilidad.
 ;; entrada: candidate -> recibe una lista de conjuntos candidatos.
 ;; salida: booleano -> en #t en caso de ser mayor a cero, #f en caso de ser menor a cero.
 (define (viabilidad candidate)
-  (cond ((> (sum-values candidate) 0) #t)
+  (cond ((> (suma-valores candidate) 0) #t)
         (else #f)))
 ;; como usar:
 ;; (viabilidad '(-1 0 1 3 4 5))
