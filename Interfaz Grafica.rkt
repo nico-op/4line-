20b7b8c34bdfbe5eb38b5efe141b02c15263c4a7
Gabriel Lobo <kevinlobo1920k@gmail.com>
2023-03-11 23:11:52 -0600

    recorrido de i y j en la matriz de interfaz

--- Interfaz Grafica.rkt
+++ Interfaz Grafica.rkt
@@ -45,7 +45,7 @@
 (define panel1( new horizontal-panel%[parent frame_p] [alignment '(center center)]))
 
 
-;Crea los campos de texto  ;row fila ,column columna
+;Crea los campos de texto        ;row fila ,column columna
 (define tf_columnas(new text-field% [label "Numero de columnas"] [parent panel1] [min-width 10] [min-height 10] [vert-margin 10] [horiz-margin 10]))
 (define tf_filas (new text-field% [label "Numero de filas"] [parent panel1] [min-width 10] [min-height 10] [vert-margin 10] [horiz-margin 10]))
 
@@ -68,7 +68,7 @@
     [(> (string->number columnas) 16) (new message% [parent panel3] [label "Error! El numero maximo de columnas es de 16"])]
     [(< (string->number filas) 8) (new message% [parent panel3] [label "Error! El numero minimo de filas es de 8"])]
     [(< (string->number columnas) 8) (new message% [parent panel3] [label "Error! El numero minimo de columnas es de 8"])]
-    [else (Juego #t(send tf_columnas get-value)(send tf_filas get-value))(send frame_p show #f)])))   ;cierra la ventana principal y abre la de juego, obtiene los valores ingresados a los textfields
+    [else (Juego #t(send tf_columnas get-value)(send tf_filas get-value))(send frame_p show #f)])))   ;cierra la ventana principal y abre la de juego
 
 
 
@@ -92,31 +92,40 @@
         (define alto 0)  ;va a definir el alto del tablero
         (define ancho 0)  ;va a definir el ancho del tablero
 
+               
+        (define (cuadroElegir columna)
+          (cond ((equal? (solucion (+ columna 1) 1 matriz) #t) (message-box "Ganado") )
+                (else (cuadroElegir-aux (solucion (+ columna 1) 1 matriz))))
+          (send frame_juego refresh))
+        (define (cuadroElegir-aux matModificada)
+          (cond ((equal? (solucion (conexion columna) 2 matriz) #t) (message-box "Perdió"))
+                (else (set! matriz (solucion (conexion columna) 2 matModificada)))))
+ 
+                
+
         (for([i (in-range (string->number columna))])   ;segun la cantidad de columnas ese sera el ancho del tablero
           (set! ancho ( + 80 ancho)))
 
         (for([j (in-range (string->number fila))])  ;segun la cantidad de filas ese sera el alto del tablero
           (set! alto ( + 80 alto)))   ;por cada fila y columna va a añadir 80 pixeles hacia el alto y ancho de la ventana
+
 (display matriz)
 (newline)
-
-
 ; Coloca las fichas en el tablero
-(define (Colocar-ficha color posX posY dc)
+(define (SetToken color posX posY dc)
   (send dc set-brush color 'solid) ;se rellena el circulo
   (send dc set-pen color 4 'solid)
-  (send dc draw-ellipse (* 80 posY) (* 80 posX) 50 50) ;se dibuja el circulo amarillo
-  (send dc draw-ellipse (* 80 posY) (* 80 posX) 50 50)) ;se dibuja el circulo rojo
+  (send dc draw-ellipse (* 80 posY) (* 80 posX) 50 50)) ;se dibuja el circulo
 
 
 ; Con esta funcion se puede conocer la posicion del mouse, obteniendo las coordenadas en "y" y "x" de la ventana
-(define (posicion-mouse event)
+(define (mouse-pos event)
            (define-values (x) (send event get-x))
            (define-values (y) (send event get-y))
            (values x y))
 
 (define Columna 0)
-(define Seleccion #f)
+(define Selected #f)
 
 (define (GetColumna x)
   (set! Columna (quotient x 80)))
@@ -135,27 +144,17 @@
            
            (for ([i (in-range 0 (string->number fila))])
            (send dc draw-line 0 (* i 80) ancho (* i 80)))
+           
+           (for* ([i (in-range 1 (string->number fila))]
+                  [j (in-range 1 (string->number columna))])
+             (let ([val (verificaMatriz matriz i j)])
+               (cond [(= val 1) (SetToken "yellow" i (- j 1) dc)]
+                     [(= val 2) (SetToken "green" i (- j 1) dc)]))))
 
-           (cond ((equal? Seleccion #t) (Colocar-ficha "yellow" 7 1 dc) (Colocar-ficha "red" 5 1 dc))))  ;el 7,7 es donde se coloca la ficha (comienza a contar a partir de cero)
-         
 
-;Funcion que abre un mensaje informando la situacion (ganador o perdedor)
-         
-         (define (Winner Status)
-  (send frame_juego delete-child Canva)
-  (cond [(equal? Status 1)
-         (new message% [parent frame_juego]
-                       [label "Ha ganado la partida :)"])]
-        [(equal? Status 0)
-         (new message% [parent frame_juego]
-                       [label "Ha perdido la partdida :("])]
-        [(equal? Status 2)
-          (new message% [parent frame_juego]
-                       [label "Hay un empate"])]
-        ))    
+           ;(cond ((equal? Selected #t) (SetToken "yellow" 7 1 dc))))  ;el 7,7 es donde se coloca la ficha (comienza a contar a partir de cero)
+           ;(define (SetToken color posX posY dc)
 
-         
-         
 ;En este canvas se va a ir refrescando la pantalla de juego con los nuevos cambios o eventos, y se hace un enlace de las coordenadas "x" y "y" reales del mouse
 ;cuando el usuario vaya haciendo click en el canva
 
@@ -164,8 +163,8 @@
                               (define/override (on-event event)
                                 (match (send event get-event-type)
                                   ['left-down
-                                   (let-values (((x y) (posicion-mouse event)))
-                                     (GetColumna x) (displayln Columna) (set! Seleccion #t) (send frame_juego refresh) (send Canva flush))]
+                                   (let-values (((x y) (mouse-pos event)))
+                                     (GetColumna x) (displayln Columna) (set! Selected #t) (send frame_juego refresh) (send Canva flush))]
                                   [else (void)]))))
 
          ; Se crea un canvas nuevo donde se van a colocar todos los elementos graficos, lineas, solidos
@@ -179,6 +178,13 @@
 ))
 
 
+;(require (lib "graphics.ss" "graphics")) ;libreria de graficos simples
+;(open-graphics) ;aqui se llama a la libreria
+;(define v_principal (open-viewport "Ventana Principal" 700 600))   ;;se crea la ventana principal, y se pasan los parametros (ancho,alto)
+;(define oculta (open-pixmap "Ventana Principal" 700 600))  ; crea una ventana oculta
+;;(define imagen-fondo (bitmap/file "C:/Users/cecil/OneDrive/Documentos/4line-/2.png"))
+;(define panel (new horizontal-panel% [parent frame]))
+;(define canvas (new canvas% [parent panel]))
 
 
 
